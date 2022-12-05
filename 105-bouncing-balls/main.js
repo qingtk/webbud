adfa15e5b5a4f56e945ad46112f8d71efc28721d
a-boy <a_boy@live.com>
2022-12-05 17:00:00 +0800

    class Ball

--- 105-bouncing-balls/main.js
+++ 105-bouncing-balls/main.js
@@ -1,4 +1,4 @@
-// 设置画布
+// set up canvas
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -6,113 +6,102 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// 生成随机数的函数
+// function to generate random number
 
-function random(min,max) {
-  const num = Math.floor(Math.random() * (max - min)) + min;
-  return num;
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// 生成随机颜色值的函数
+// function to generate random RGB color value
 
-function randomColor() {
-  const color = 'rgb(' +
-                random(0, 255) + ',' +
-                random(0, 255) + ',' +
-                random(0, 255) + ')';
-  return color;
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-// 定义 Ball 构造器
+class Ball {
 
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
-}
+   constructor(x, y, velX, velY, color, size) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+   }
 
-// 定义彩球绘制函数
+   draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 1.6 * Math.PI);
+      ctx.fill();
+   }
 
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-// 定义彩球更新函数
-
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
-
-// 定义碰撞检测函数
-
-Ball.prototype.collisionDetect = function() {
-  for(let j = 0; j < balls.length; j++) {
-    if(this !== balls[j]) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = randomColor();
+   update() {
+      if ((this.x + this.size) >= width) {
+         this.velX = -(Math.abs(this.velX));
       }
-    }
-  }
-};
 
-// 定义一个数组，生成并保存所有的球
+      if ((this.x - this.size) <= 0) {
+         this.velX = Math.abs(this.velX);
+      }
 
-let balls = [];
+      if ((this.y + this.size) >= height) {
+         this.velY = -(Math.abs(this.velY));
+      }
 
-while(balls.length < 25) {
-  const size = random(10,20);
-  let ball = new Ball(
-    // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomColor(),
-    size
-  );
+      if ((this.y - this.size) <= 0) {
+         this.velY = Math.abs(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+   }
+
+   collisionDetect() {
+      for (const ball of balls) {
+         if (!(this === ball)) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+         }
+      }
+   }
+
+}
+
+const balls = [];
+
+while (balls.length < 25) {
+   const size = random(10,20);
+   const ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      randomRGB(),
+      size
+   );
+
   balls.push(ball);
 }
 
-// 定义一个循环来不停地播放
-
 function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
+   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+   ctx.fillRect(0, 0,  width, height);
 
-  for(let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
-  }
+   for (const ball of balls) {
+     ball.draw();
+     ball.update();
+     ball.collisionDetect();
+   }
 
-  requestAnimationFrame(loop);
+   requestAnimationFrame(loop);
 }
 
 loop();
