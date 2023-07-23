4e63409f322163c11dd7e67809bb71674853ed75
a-boy <a_boy@live.com>
2023-07-24 07:53:45 +0800

    grid & triangle

---
+++ 36-grid-layout/script2.js
@@ -0,0 +1,13 @@
+/*
+Firefox has the best developer tools for building with CSS Grid Layout
+Ready to step up your game with CSS Grid? Just follow these steps:
+
+Step 1: Download Firefox Nightly
+https://www.mozilla.org/en-US/firefox/channel/desktop/#nightly
+
+Step 2: While using Nightly, open Developer Tools
+Windows: Ctrl + Shift + I
+Mac: Cmd + Opt + I
+
+Step 3: Select the layout panel, and then select the overlay grid you'd like to display
+*/
---
+++ draft/test-JavaScript-snippets.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+for (const item of object) {
+    
+}
